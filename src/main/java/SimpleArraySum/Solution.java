de6134a323c05ea0f55811a011fc042d162438d9
matim <mati.matias09@gmail.com>
2023-07-30 12:37:55 +0200

    SimpleArraySum

---
+++ src/main/java/SimpleArraySum/Solution.java
@@ -0,0 +1,39 @@
+package SimpleArraySum;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+
+
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        for (Integer x : ar) {
+            sum +=x;
+        }
+        return sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
