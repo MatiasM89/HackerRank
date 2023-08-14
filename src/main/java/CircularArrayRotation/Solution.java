442ba9a71f5bdd51a1276c0a6b94bbcd3896478d
matim <mati.matias09@gmail.com>
2023-08-14 12:23:27 +0200

    CircularArrayRotation

---
+++ src/main/java/CircularArrayRotation/Solution.java
@@ -0,0 +1,72 @@
+package CircularArrayRotation;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static List<Integer> circularArrayRotation(List<Integer> a, int k, List<Integer> queries) {
+//        LinkedList<Integer> rotation = new LinkedList<>(a);
+//
+//        for (int i = 0; i < k; i++) {
+//            int last = rotation.get(rotation.size() - 1);
+//            for (int j = rotation.size() - 1; j > 0; j--) {
+//                rotation.set(j, rotation.get(j - 1));
+//            }
+//            rotation.set(0, last);
+//        }
+
+        List<Integer> results = new LinkedList<>();
+
+        for (Integer x : queries) {
+            results.add(a.get(Math.floorMod(x - k, a.size())));
+        }
+        return results;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int q = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> queries = IntStream.range(0, q).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.circularArrayRotation(a, k, queries);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
