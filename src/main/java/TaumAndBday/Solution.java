bf75dafd6f6c5c29b6e3b7b5f3182ce360cf70d2
panboleslaw89 <mati.matias09@gmail.com>
2023-08-25 11:25:18 +0200

    TaumAndBday

---
+++ src/main/java/TaumAndBday/Solution.java
@@ -0,0 +1,49 @@
+package TaumAndBday;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.stream.*;
+
+class Result {
+    public static long taumBday(int b, int w, int bc, int wc, int z) {
+        return b * Math.min(bc, (long) (wc + z)) + w * Math.min(wc,(long) (bc + z));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int b = Integer.parseInt(firstMultipleInput[0]);
+
+                int w = Integer.parseInt(firstMultipleInput[1]);
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int bc = Integer.parseInt(secondMultipleInput[0]);
+
+                int wc = Integer.parseInt(secondMultipleInput[1]);
+
+                int z = Integer.parseInt(secondMultipleInput[2]);
+
+                long result = Result.taumBday(b, w, bc, wc, z);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
