c31cd9231de561f0070cefa745ff9fec944f1a37
panboleslaw89 <mati.matias09@gmail.com>
2023-08-30 11:42:30 +0200

    ChocolateFeast

---
+++ src/main/java/ChocolateFeast/Solution.java
@@ -0,0 +1,49 @@
+package ChocolateFeast;
+
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+    public static int chocolateFeast(int n, int c, int m) {
+        int wrappers = n / c;
+        int counter = n / c;
+        while (wrappers >= m) {
+            wrappers -= m;
+            counter++;
+            wrappers++;
+        }
+        return counter;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int c = Integer.parseInt(firstMultipleInput[1]);
+
+                int m = Integer.parseInt(firstMultipleInput[2]);
+
+                int result = Result.chocolateFeast(n, c, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
