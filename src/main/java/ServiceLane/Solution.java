53d5dbc3b7994da59b9f6ee1c0d1e49622ad445a
panboleslaw89 <mati.matias09@gmail.com>
2023-08-30 17:30:01 +0200

    ServiceLane

---
+++ src/main/java/ServiceLane/Solution.java
@@ -0,0 +1,68 @@
+package ServiceLane;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static List<Integer> serviceLane(int n, List<List<Integer>> cases, List<Integer> width) {
+        List<Integer> list = new ArrayList<>();
+        for (List<Integer> caseList : cases) {
+            int max = 3;
+            for (int i = caseList.get(0); i <= caseList.get(1); i++) {
+                if (width.get(i) < max) {
+                    max = width.get(i);
+                }
+            }
+            list.add(max);
+        }
+        return list;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> width = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<List<Integer>> cases = new ArrayList<>();
+
+        IntStream.range(0, t).forEach(i -> {
+            try {
+                cases.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.serviceLane(n, cases, width);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
